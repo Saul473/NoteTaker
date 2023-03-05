68a1239a8947ca6741f64315fac2ba0620b2b522
Saul473 <118771655+Saul473@users.noreply.github.com>
2023-03-05 16:14:34 -0700

    finished adding routes, adjusted package-lock.json, fixed package.json

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 var fs = require('fs');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>
+app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-app.get('/', (req, res)=>
+app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/api/notes', (req, res)=>{
     try {
-        res.send(JSON.parse(fs.readFileSync('db/db.json')))
+        res.send(JSON.parse(fs.readFileSync('./Develop/db/db.json')))
     } catch {
         console.log('there has been an error!');
         res.sendStatus(500);
@@ -24,8 +27,21 @@ app.get('/api/notes', (req, res)=>{
 });
 
 app.post('/api/notes', (req, res)=>{
-    let notes = JSON.parse(fs.readFileSync('db/db.json'))
+    let notes = JSON.parse(fs.readFileSync('./Develop/db/db.json'))
     notes.push({
-        
+        "id": uuidv4(),
+        "title": req.body.title,
+        "text": req.body.text
     })
-})
+    fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err)=>{
+        if(err){
+            console.log('there has been an error!');
+            res.sendStatus(500);
+        }
+    })
+    res.sendStatus(200);
+});
+
+
+
+app.listen(PORT, () => console.log(`app running on port ${PORT} 🥸`))
