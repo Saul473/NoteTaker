db8549a1ed8b5bb5f704e448bdc68c514e87fdc1
Saul473 <118771655+Saul473@users.noreply.github.com>
2023-03-05 13:35:56 -0700

    added routes for index.html and index.html

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-helll test test test
+var fs = require('fs');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/', (req, res)=>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+module.exports = function(app){
+    app.get('/api/notes', (req, res)=>{
+        console.log('execute GET notes request');
+    })
+}
